b4dbf19b7ea63e5678fe9e296ce5e8edef0804cf
Ben from NES Universe <benissoflippinghot@gmail.com>
2023-03-25 11:59:45 +0000

    Reformatted "make"
    Modified modifier to use newer kwarg method

--- src/ipsluna/ips/__init__.py
+++ src/ipsluna/ips/__init__.py
@@ -43,15 +43,33 @@ class ips:
             self.end = self.offset + self.size
 
 
-        def modify(self, offset : int = None, data : bytes | bytearray | tuple = None, name : str = None, override : bool = False, sustain : bool = True):
+        def modify(self, **kwargs):
+            """
+            *offset = modify offset | if not an arg then no modification is made
+            *data = modify data     | if not an arg then no modification is made
+            *name = modify name     | if not an arg then no modification is made    | If set to None then new name will be made from new offset
 
-            if self.name != name and not name is None:                              #if attempting re-name
-                if isinstance(name,str): self.name = name                           #if legal, perform
-                else: raise TypeError("Given name is not suitable as it is not str")#else raise TypeError
+            *override = overriding existing data
+            *sustain  = whilst overriding, attempt to maintain as much mod data as possible
+            """
+            valid_args = {"offset", "data", "name", "override", "sustain"}
+            if not all(arg in valid_args for arg in kwargs):
+                raise ValueError(f"Invalid arguments provided: {set(kwargs.keys()) - valid_args}")
+    
 
-            for resolve in ["offset", "data", "name"]:                                                    #of all jobs to do
-                if eval(resolve) is None: eval(f"{resolve} = self.{resolve}")       #define locals by equal attributes
+            defaults = {"override": False, "sustain": True}
+            offset = kwargs.get("offset", self.offset)
+            data = kwargs.get("data", self.data)
+            name = kwargs.get("name", self.name)
 
+            if name is None:  
+                name = f"Unnamed instance at : {offset}"
+
+            override = kwargs.get("override", defaults["override"])
+            sustain = kwargs.get("sustain", defaults["sustain"])
+
+
+           
             clashes = super().in_range(super().in_range(end=offset)[-1].end, offset+(data[1] if isinstance(data,tuple) else len(data))) 
             #retrieve all instances of the parent class from the start of the offset to the end
             if len(clashes):    #if not zero-length
@@ -67,7 +85,11 @@ class ips:
                 else: raise ScopeError("Space is already occupied by other instances")  #otherwise raise ScopeError due to impossible task
             self.offset, self.data,self.name = offset, data, self.name if name is None else name   #gathered that no errors rose, finish the data
             self.rle = isinstance(self.data,tuple)
-            #could we just do self.__init__(self, offset, data, name)?      
+            #could we just do self.__init__(self, offset, data, name)?     
+            #
+            if self.name != name and not name is None:                              #if attempting re-name
+                if isinstance(name,str): self.name = name                           #if legal, perform
+                else: raise TypeError("Given name is not suitable as it is not str")#else raise TypeError 
              
             super().instances.remove(self)                                                      #remove
             temp = super().instances.index(super().in_range(self.offset)[0])                    #from start = self.offset, end = (2**super().bitsize)-1 DEF
@@ -267,7 +289,87 @@ def apply(patch : ips, base : bytes | bytearray) -> bytes:
             return build+ base[len(build):]
         except:
             raise Exception("ips class contains impossible data")
- 
+
+
+def make(base : bytes | bytearray, modded : bytes | bytearray, bitsize : int = 24, legal : bool = None) -> bytes:
+
+    #make it create `ips` class
+
+    if len(modded) > min(0xFFFFFF,(2**bitsize)-1): raise Exception("Scope impossible")
+    ipsf,count = b"",0
+    while count != len(modded):
+        data = 0
+		
+		#The 5 is crucial for rle viability checks with optimization
+        if count  + 5 < len(base) and legal:
+		#do not add matches ever
+		
+            while base[count] is modded[count]:
+                count += 1
+				
+			#if we can't find a match in the next 5 bytes, prefer rle
+            if True not in [base[x] is modded[x] for x in range(5)] and bytes(modded[count])*5 is modded[count:count+5]:
+				
+				#check consecutive bytes
+                while modded[count + data] is modded[count] if len(base) > count + data + 1 else True:
+                    data += 1
+                data = (data//0xFFFF,data%0xFFFF)
+                for split in range(data[0]):
+                    ipsf += count.to_bytes(3,"big")+b"\x00\x00\xff\xff"+bytes([count])
+                    count += 0xFFFF
+                ipsf += count.to_bytes(3,"big")+data[1].to_bytes(2,"big")+bytes([count])
+                count += data[1].to_bytes
+				
+					
+					
+				
+            else:
+                    #for each failed match
+                    while not base[count] is modded[count] if len(base) > count + data + 1 else True:
+                        if bytes([modded[count + data]])*5 is modded[count:count+data+5]: break 
+                        data += 1
+                    data = data // 0xFFFF, data % 0xFFFF
+
+                    for split in range(data[0]):
+                        ipsf += count.to_bytes(3, "big")+b"\xff\xff"+modded[count:count+0xFFFF] 
+                        count += 0xFFFF 
+                    ipsf += count.to_bytes(3, "big")+data[1].to_bytes(2,"big")+modded[count:count+data[1]] 
+                    count += data[1]
+				    
+
+
+				#split data every 16 bit upperlimit 
+				#pass #!!!!! Important
+
+				
+				
+        else:
+			#do not append zerodata
+            while not modded[count]:
+                count += 1
+
+			#is rle viable/possible form this offset
+            if bytes([modded[count]])*5 is count[count:count+5]:
+                while modded[count + data] is modded[count]:
+                    data += 1
+                data = (data//0xFFFF,data%0xFFFF)
+                for split in range(data[0]):
+                    ipsf += count.to_bytes(3,"big")+b"\x00\x00\xff\xff"+bytes([count])
+                    count += 0xFFFF
+                ipsf += count.to_bytes(3,"big")+data[1].to_bytes(2,"big")+bytes([count])
+                count += data[1].to_bytes
+				
+			#granted rle is unviable, check for viability throughout constructor
+            else:
+                while not bytes([modded[count]])*5 is count[count:count+5]:
+                    data += 1
+				#split data every 16 bit upperlimit 
+                pass #!!!!! Important
+
+		#are we needing to write a length diff
+        if count is len(modded)-1:
+            ipsf += count.to_bytes(3,"big")+b"\x00\x00\x00\x01\x00"
+    return ips(ipsf)	
 
 
   #Code is still faulty
