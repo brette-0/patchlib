b701ded36c0d9280eea84338fd15c02c9bfb8760
Ben from NES Universe <benissoflippinghot@gmail.com>
2023-04-11 17:37:42 +0100

    Removed `0.0.0` file generation

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'patchlib'
 copyright = '2023, BrettefromNesUniverse'
 author = 'BrettefromNesUniverse'
-release = '1.0'
+release = '1.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
