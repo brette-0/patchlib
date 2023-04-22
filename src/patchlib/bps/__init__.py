26d20822edbe3d585271bc6bdcfb521a0e2d780a
Ben from NES Universe <benissoflippinghot@gmail.com>
2023-04-22 21:43:14 +0100

    Cut the ChatGPT stuff
    After learning that ChatGPT struggles horrifically with context the entire methodology for this has changed.
    
    Encode and Decode now function completely, trim function can be confirmed to work.
    Slight logical adjustments to Checksum retrieval

--- src/patchlib/bps/__init__.py
+++ src/patchlib/bps/__init__.py
@@ -2,99 +2,123 @@
 experimental alpha bps handler
 """
 
-__version__ = "0.1"
+__version__ = "0.3"
 
-def encode(data : int) -> bytearray:  #create function return bytearray, takes int
+def encode(number : int) -> bytes:
+    """
+	Convert number into variable-width encoded bytes
+    """
 
-    encoded = b""                       #create bytearray for return
-    while True:                         #create indefinte loop
-        x = data & 0x7f                 #create temporary variable "x" with "content" of byte | taking it's lower 7LSBs
-        data >>= 7                      #trim last 7 bytes off
-        if data == 0:                   #if data is equivalent to zero, write signed MSB + stored 7 LSBs
-            encoded+=bytes([0x80 | x])  
-            break                       #exit loop
-        encoded+=bytes([x])             #else if not end, loop after appending stored 7lSBs with
-        data-=1                         #decrement at the end
-    return encoded                      #return bytearray of encoded contents
+    if not isinstance(number,int): raise TypeError("Number data must be integer")
+    if number < 0: raise ValueError("Cannot convert negative number!")
+	
+    var_length = b""							#Create empty bytes object to store variable-width encoded bytes
+    while True:									#Until we are finished
+        x = number & 0x7f						#Using the 7 LSB of the number
+        number >>= 7							#And removing such data
+        if number: 								#If there is still source data to read
+            var_length += x.to_bytes(1, "big")	#Append such data to our bytes object
+            number -= 1							#Decrement by one to remove ambiguity
+        else: 
+            var_length += (0x80 | x).to_bytes(1, "big")	#Set termination byte
+            break 										#Final data is wronte, leave
+    return var_length									#return our variable-width encoded data
 
 
-def decode(Bytes: bytes) -> int:
-    data, shift = 0, 1
-    index = 0
-    while True:
-        x = Bytes[index]
-        data += (x & 0x7f) * shift
-        index += 1
-        if x & 0x80:
-            break
+def decode(encoded : bytes) -> int:
+    """
+	Decode variable-width encoded bytes into unsinged integer
+    """
+    if not isinstance(encoded,bytes): raise TypeError("Encoded data must be bytes object")
+
+    number = 0								    #Assume 0 for operational efficiency 
+    shift = 1
+    for byte in encoded:					    #for each byte in our given data
+        number += (byte & 0x7f) * shift		    #increase number by data bits
+        if byte & 0x80: break				    #break if termination bytes set
         shift <<= 7
-        data += shift
-    return data
-
+        number += shift
+    return number							    #return decoded number
 
 class bps:
     class SourceRead: 
         def __init__(self, parent, length): self.parent,self.length = parent, length
     class TargetRead: 
         def __init__(self, parent, length): self.parent,self.length = parent, length
-    class SourceCopy: pass
-    class TargetCopy: pass
+    class SourceCopy: 
+         def __init__(self, parent, length, relativeOffset): self.parent,self.length,self.relativeOffset = parent, length, relativeOffset
+    class TargetCopy: 
+        def __init__(self, parent, length, relativeOffset): self.parent,self.length,self.relativeOffset = parent, length, relativeOffset
 
-    def __init__(self, source : bytes):
+    def __init__(self, patch : bytes):
 
-        trim = lambda: source[source.index([byte for byte in source[:16] if byte & 128][0])+1:]   #trim bps from first d0 enabled byte
+        trim = lambda: patch[patch.index([byte for byte in patch[:16] if byte & 0x80][0])+1:]   #trim bps from first d0 enabled byte
         
-        normalized = {};bps = bps[4:]
-        normalized["Source Size"] = decode(bps[:16]);source = trim()    #Decode Source Size   | read 16 bytes ahead for u128i
-        normalized ["Target Size"] = decode(bps[:16]);source = trim()   #Decode Target Size   | read 16 bytes ahead for u128i
-        normalized ["Metadata Size"] = decode(bps[:16]);source = trim() #Decode Metadata Size | read 16 bytes ahead for u128i
+        patch = patch[4:]
+        self.source_size = decode(patch[:16]);patch = trim()     #Decode Source Size   | read 16 bytes ahead for u128i
+        self.target_size = decode(patch[:16]);patch = trim()     #Decode Target Size   | read 16 bytes ahead for u128i
+        self.metadata_size = decode(patch[:16]);patch = trim()   #Decode Metadata Size | read 16 bytes ahead for u128i
         
     
-        normalized["Metadata"] = bps[:normalized["Metadata Size"]]              #Gather Metadata as bytearray (should be xml | may not be)
-        source = source[normalized["Metadata Size"]:]                                 #trim bps to exclude metadata
+        self.metadata = patch[:self.metadata_size]             #Gather Metadata as bytearray (should be xml | may not be)
+        patch = patch[self.metadata_size:]                    #trim bps to exclude metadata
 
-        normalized["source-checksum"] = source[-24:-16]                            #Source file checksum
-        normalized["target-checksum"] = source[-16:-8]                             #Target file checksum 
-        normalized["patch-checksum"] = source[-8:]                                 #bps file checksum
+        self.source_checksum = patch[-12:-8]
+        self.target_checksum = patch[-8:-4]
+        self.patch_checksum = patch[-4:]
 
-        source = source[:-24]                                                         #trim Footer
+        normalized = {}
+         
+        data_offset = 0
+        while data_offset < len(patch):
+            data = decode(patch[data_offset:])
+            command = data & 3
+            length = (data >> 2) + 1
+
+            if command & 2:  # if command is 2 or 3, read relative_offset
+                relative_offset = decode(patch[data_offset + 1:])
+                data_offset += 2
+
+            normalized[data_offset] = command, (length, data_offset) if command & 2 else (length,)
+            data_offset += 1
+        self.actions = [(self.SourceRead, self.TargetRead, self.SourceCopy, self.TargetRead)[action[0]](*action[1]) for action in normalized]
+
         
 
 
 
 
-#Some chatGPT churned out code, yet to modify
-def sourceCopy(length: int) -> bytes:
-    global source, sourceOffset
-    if sourceOffset + length > len(source):
-        raise ValueError("Invalid source offset")
-    data = source[sourceOffset:sourceOffset+length]
-    sourceOffset += length
-    return data
 
-def targetCopy(length: int) -> bytearray:
-    global target, targetOffset
-    data = bytearray(target[targetOffset:targetOffset+length])
-    targetOffset += length
-    return data
+def apply(patch: bytes, source: bytes) -> bytes: 
+    """
+    Applies BPS Patch to source file
+    """
 
-def sourceCopy(relativeOffset: int, length: int) -> None:
-    global source_offset, target_offset
+    target = bytearray(source)
+    source_checksum, target_checksum, patch_checksum = [patch[i:i+4] for i in range(-12, 0, 4)];patch = patch[4:-12]
+    trim = lambda: patch[patch.index([byte for byte in patch[:16] if byte & 128][0])+1:]   #trim bps from first d0 enabled byte
 
-    source_offset += relativeOffset
-    if source_offset < 0 or source_offset + length > len(source):
-        raise ValueError("Invalid source offset")
+    source_size = decode(patch[:16]); patch = trim()                    # Decode Source Size   | read 16 bytes ahead for u128i
+    target_size = decode(patch[:16]); patch = trim()                    # Decode Target Size   | read 16 bytes ahead for u128i
+    metadata_size = decode(patch[:16]); patch = trim()                  # Decode Metadata Size | read 16 bytes ahead for u128i
 
-    target[target_offset:target_offset+length] = source[source_offset:source_offset+length]
-    target_offset += length
+    if metadata_size:
+        metadata = patch[:metadata_size];patch = patch[metadata_size:]  # Gather Metadata as bytearray (should be xml | may not be)
 
 
-def targetCopy(relativeOffset: int, length: int) -> None:
-    global target_offset
+    def source_read() -> None:
+        pass #nonlocal some variables and perform some function
 
-    target_offset += relativeOffset
-    if target_offset < 0 or target_offset + length > len(target):
-        raise ValueError("Invalid target offset")
+    def target_read() -> None:
+        pass #nonlocal some variables and perform some function
 
-    target[target_offset:target_offset+length] = target[target_offset-target_offset:target_offset+length-target_offset]
-    target_offset += length
+
+    def source_copy() -> None:
+        pass #nonlocal some variables and perform some function
+
+    def target_copy() -> None:
+        pass #nonlocal some variables and perform some function
+
+    #Here we shall haev the code to loop instructions
+
+    return target, source_checksum, target_checksum, patch_checksum, metadata
